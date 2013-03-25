5d62ad9512a5c87fc77ba3d71712481d508b896f
Irfan Pirbhai <irfan.pirbhai@gmail.com>
2013-03-24 20:55:43 -0400

    Switch code from static_pages_helper to application_helper

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,12 +1,3 @@
 module StaticPagesHelper
 
-    def full_title(page_title)
-      base_title = 'RoR Tutorial Sample App'
-      if page_title.empty?
-        base_title  
-      else
-        "#{base_title} | #{page_title}"
-      end
-    end
-
 end
